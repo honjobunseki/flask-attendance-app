d65329b1469dff5c7a8df4d578ef9291ce4f8954
honjobunseki <mail@asbestos-kensa.com>
2025-01-20 13:33:13 +0900

    Update Flask app.py

--- Flask app.py
+++ Flask app.py
@@ -175,4 +175,5 @@ def send_email():
     return redirect(url_for("calendar"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
